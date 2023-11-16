7c7ab5a7751d09a96f9b5b27737a13ec7a4f82e2
root <root@Harsh-Arora>
2023-11-16 14:49:47 +0530

    Initial commit.

---
+++ src/SuspiciousPrj_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { SuspiciousPrj_backend } from "../../declarations/SuspiciousPrj_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await SuspiciousPrj_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
